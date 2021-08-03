0b13f603bbe5b1dbea141dc19b0122c55bd5b211
Mariano Meissner <marianome@gmail.com>
2021-08-03 17:56:36 +0200

    first commit

---
+++ src/Hello.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Elitelib;
+
+class Hello{
+
+    public function sayHello($message){
+
+        echo "hello $message";
+    }
+
+}
---
+++ test/index.php
@@ -0,0 +1,8 @@
+<?php
+
+include __DIR__."/../vendor/autoload.php";
+
+
+$hello = new \Elitelib\Hello();
+
+$hello->sayHello("composer");
